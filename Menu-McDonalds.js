5c8dda43cd10b3ac56a4de025295db5b6762dc34
GeumarPerez <geumarprez@gmail.com>
2022-11-28 00:45:12 -0400

    second commit

---
+++ Menu-McDonalds.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+	var menuToggle=document.querySelector('.header__div');
+	var nav=document.querySelector('.header__ul')
+	menuToggle.classList.toggle('active')
+	nav.classList.toggle('active')
+}
